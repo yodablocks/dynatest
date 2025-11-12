ca01d8af74d20a8f8c3d96fcfe45758aad5979e3
MLiserb <yrismarc@proton.me>
2025-11-12 17:26:53 +0800

    fixing image issue

--- src/constants/protocols/ankr.ts
+++ src/constants/protocols/ankr.ts
@@ -11,7 +11,7 @@ export const ANKR = {
   name: "Ankr",
   description:
     "Ankr is a liquid staking protocol that allows users to stake assets while maintaining liquidity.",
-  icon: "/crypto-icons/protocol/ankr.png",
+  icon: "/crypto-icons/protocol/ankr.svg",
   link: "https://www.ankr.com/",
   contracts: ANKR_CONTRACTS,
 } as const satisfies Protocol;
